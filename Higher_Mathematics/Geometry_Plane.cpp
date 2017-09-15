bc7229614657a70c726a6d446f3cef17c07ec99a
Kaspar <maggot99999@gmail.com>
2017-09-15 10:09:52 +0100

    Removed unnecessary files

--- Higher_Mathematics/Geometry_Plane.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Geometry_Plane.cpp
-//  Higher_Mathematics
-//
-//  Created by Eliza on 15/09/2017.
-//  Copyright © 2017 maggot99999. All rights reserved.
-//
-
-#include "Geometry_Plane.hpp"
